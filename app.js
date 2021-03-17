b4cb3b9dabecc9cf10ccd7df23362f2673f6b523
joordy <jmfronik@gmail.com>
2021-03-17 13:49:54 +0100

    Added route

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log('hi')
-
 const express = require('express'),
   app = express(),
   path = require('path'),
--- src/router/routes.js
+++ src/router/routes.js
@@ -9,12 +9,14 @@ const { home } = require('./../renders/home')
 const { shop } = require('./../renders/shop')
 const { product } = require('./../renders/product')
 const { cart } = require('./../renders/cart')
+const { profile } = require('./../renders/profile')
 
 // Setting get routes
 router.get('/', home)
 router.get('/shop', shop)
 router.get('/product/:id', product)
 router.get('/cart', cart)
+router.get('/profile', profile)
 
 // Setting post routes
 router.post('/cart', cart)
